12f801a5d4f85869f1b81a7a96fbaf769564ddb4
JOHN CHURCHILL <JOHN.CHURCHILL@maryland.gov>
2020-06-18 15:02:39 -0400

    build: edits to comments primarily. New read_df_by_date fnxn.

--- time_series_merge.py
+++ time_series_merge.py
@@ -3,13 +3,12 @@ import arcpy
 import pandas
 import fclist
 
-#date_list and fc_date_lookup are both not being used yet.
 
 fd_name = "v1"
 out_ws = r"C:\Users\john.churchill\Documents\PROJECTS\Recovery_Dashboard\TS_seven_day.gdb"
 template_fc = r"C:\Users\john.churchill\Documents\PROJECTS\Recovery_Dashboard\MasterCaseTracker_TimeSeries.gdb\Template_County_FC_v2"
 
-# ToDo TEST FIRST TO SEE IF fd EXISTS
+# Test First to see if fd exists
 if not arcpy.Exists(os.path.join(out_ws, fd_name)):
     arcpy.CreateFeatureDataset_management(out_ws, fd_name)
 
@@ -17,57 +16,58 @@ def create_fc(fc):
     #arcpy.CreateFeatureClass_management(fc)
     arcpy.CopyFeatures_management(template_fc, os.path.join(out_ws, fd_name, fc))
 
-
 def populate_fc(fc, date_df):
+    # uses a subset pandas dataframe to populate a feature class
+    # with Cases and Date values by County and Date
+    this_fc = os.path.join(out_ws, fd_name, fc)
     field_names = ["County", "Cases", "Date"]
-    with arcpy.da.UpdateCursor(fc, field_names) as upd_cursor:
+    with arcpy.da.UpdateCursor(this_fc, field_names) as upd_cursor:
         for row in upd_cursor:
-            #value_dict = {}
             for index, dfrow in date_df.iterrows():
                 if row[0] == dfrow['jurisdiction']:
                     row[1] = dfrow['percent']
                     row[2] = dfrow['date']
                     upd_cursor.updateRow(row)
-                #value_dict[dfrow['jurisdiction']] = (dfrow['date'], dfrow['percent'])
-                #print(dfrow['jurisdiction'])
-            #row[1] = value_dict[dfrow[0][1]] # percent
-            #row[2] = value_dict[dfrow[0][0]] # date
-            #upd_cursor.updateRow(row)
-            
-            # go through the pandas subset dataframes
-        #for name in fc_names:
-        #    print(name)
 
 def create_fcs_from_template():
     # uses a template feature class and a list of and 
     # feature class names to create multiple copies of the template.
     for fc_name in fclist.fc_names:
         create_fc(fc_name)
+    return True
 
 def create_df(df, datestring):
     # can send the master_df and a single date (type=string)
     # returns the subset dataframe to use with the update cursor (populate_fc function)
-    date_df = df['date']=='2020-03-19'
+    date_df = df['date']==datestring
     date_df_only = df[date_df]
     return date_df_only
 
-# call populate_fc on each layer created
-#def setup_data(datelist):
-#    # SHOULD create and return a large dictionary of all percent values for jurisdiction by date
-#    # master_df is the master dataframe (the entire spreadsheet)
-# master_dictionary = {}
-master_df = pandas.read_excel(open(r'C:\Users\john.churchill\Documents\PROJECTS\Recovery_Dashboard\Daily_Positivity_Data_by_Jurisdiction.xlsx', 'rb'), sheet_name = 'Sheet2')
-#    return master_df
-#    ####df_3_19 = master_df['date']=='2020-03-19'
-#    ####df_3_19_only = master_df[df_3_19]
-#    ####for index, row in df_3_19_only.iterrows():
-#    ####    print(row['jurisdiction'] + " - " + str(row['percent']))
 
-#my_master_df = setup_data(full_datelist)
-# TEST WITH THIS AND THEN REPLACE short_date_list with fclist.date_list
-# ALSO replace print() with a call to populate_fc()!!
-short_date_list = ["2020-03-16", "2020-03-17"]
-for item in short_date_list:
-    my_df = create_df(master_df, item)
-    for index, row in my_df.iterrows():
+master_df = pandas.read_excel(open(r'C:\Users\john.churchill\Documents\PROJECTS\Recovery_Dashboard\Daily_Positivity_Data_by_Jurisdiction.xlsx', 'rb'), sheet_name = 'Sheet2')
+
+def read_df_by_date(datestring):
+    # Uses global <master_df> to create subset df and print values.
+    df = master_df['date']==datestring
+    df_only = master_df[df]
+    for index, row in df_only.iterrows():
         print(row['jurisdiction'] + " - " + str(row['percent']))
+
+# Used for testing
+# short_date_list = ["2020-03-16", "2020-03-17"]
+
+def process_all_in_list(thelist):
+    # runs the populate_fc fnxn on all fcs in a list
+    for item in thelist:
+        my_fc = fclist.fc_date_lookup[item]
+        my_df = create_df(master_df, item)
+        populate_fc(my_fc, my_df)
+
+# OPTION A Uncomment the next line to create new fcs
+it_worked = create_fcs_from_template() # generates n feature 
+# classes (fcs) from a template fc
+if it_worked:
+    process_all_in_list(fclist.date_list)
+
+# OPTION B do it anyway (uncomment the next line)
+#process_all_in_list(fclist.date_list)
